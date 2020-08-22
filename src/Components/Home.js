ae267195a91c0691d3bfaff9b48f634ea659042f
Sam <inviswings@gmail.com>
2020-08-22 16:54:36 -0700

    home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,14 +5,15 @@ const Home = () => {
     return (
         <div className="Home" id="home">
             <span className="desc">
-                <h1>
-                    I am&nbsp;
-                    <h1 style={{ fontSize: "100px", display: "inline" }}>
-                        Sam Nye
-                    </h1>{" "}
-                    <h1 style={{ textAlign: "center" }}>the VR guy</h1>
-                </h1>
-                <h1> I work to make dreams come true</h1>
+                <span id="firstLine">
+                    <h2>I am&nbsp;</h2>
+                    <h1 className="myName">Sam Nye&nbsp;</h1>
+                    <h2 className="myRole">the VR guy</h2>
+                </span>
+                <span id="secondLine">
+                    <h2>I work to make dreams&nbsp;</h2>
+                    <h2 className="fall">come true</h2>
+                </span>
             </span>
             <div className="buttons">
                 <button className="work">View My Work</button>
